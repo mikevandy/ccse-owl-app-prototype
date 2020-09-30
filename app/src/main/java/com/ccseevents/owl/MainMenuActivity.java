448ec3e108d32dad233654e537c4874cd11b9b6c
work-turtle <56421184+work-turtle@users.noreply.github.com>
2020-09-30 01:21:22 -0500

    Added the Events List Activity, it is currently empty.

--- app/src/main/java/com/ccseevents/owl/MainMenuActivity.java
+++ app/src/main/java/com/ccseevents/owl/MainMenuActivity.java
@@ -2,13 +2,25 @@ package com.ccseevents.owl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    Button buttonEventList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        buttonEventList = (Button) findViewById(R.id.button1);
+        buttonEventList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainMenuActivity.this, EventListActivity.class));
+            }
+        });
     }
 }
