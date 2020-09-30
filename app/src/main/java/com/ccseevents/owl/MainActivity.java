ab9149a3e5c411b4c6456681b98b193ac0101dc1
Scott <scottrobinson4591@gmail.com>
2020-09-29 20:29:46 -0400

    Created a button on the Main Activity to get the the Main Menu Activity

--- app/src/main/java/com/ccseevents/owl/MainActivity.java
+++ app/src/main/java/com/ccseevents/owl/MainActivity.java
@@ -2,13 +2,27 @@ package com.ccseevents.owl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.MainButton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainMenuActivity.class));
+            }
+        });
     }
 }
