2f6e690d23294c07f53622d88d460c395cfa824d
Scott <scottrobinson4591@gmail.com>
2020-10-01 12:27:42 -0400

    Fixed the splash screen so that it navigates to the main menu. Added functionality to the event list button so that it navigates to the event list activity

--- app/src/main/java/com/ccseevents/owl/MainMenuActivity.java
+++ app/src/main/java/com/ccseevents/owl/MainMenuActivity.java
@@ -6,20 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainMenuActivity extends AppCompatActivity {
-
-    Button buttonEventList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-  //    buttonEventList = (Button) findViewById(R.id.button1); will not compile    error: cannot find symbol   buttonEventList = (Button) findViewById(R.id.button1);
-        buttonEventList.setOnClickListener(new View.OnClickListener() {
+
+        ImageButton FeaturedEvent = (ImageButton)findViewById(R.id.featuredEventBtn);
+        ImageButton EventList = (ImageButton)findViewById(R.id.eventListBtn);
+        ImageButton MyEvents = (ImageButton)findViewById(R.id.myEventBtn);
+
+        EventList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainMenuActivity.this, EventListActivity.class));
+                startActivity(new Intent(MainMenuActivity.this, EventListDetailActivity.class));
             }
         });
     }
