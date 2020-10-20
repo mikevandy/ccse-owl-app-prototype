9641f50d4d135a9da4c6b3c150aeface647ed53d
Scott <scottrobinson4591@gmail.com>
2020-10-20 09:58:29 -0400

    added the calendar view of the event list and added a button to the event list activity to navigate there.

---
+++ app/src/main/java/com/ccseevents/owl/CalendarViewEventList.java
@@ -0,0 +1,14 @@
+package com.ccseevents.owl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalendarViewEventList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar_view_event_list);
+    }
+}
