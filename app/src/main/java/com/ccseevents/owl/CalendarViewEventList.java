16ae02e4f5540643eb099a4921856ee86b0ad211
Scott <scottrobinson4591@gmail.com>
2020-10-22 19:24:47 -0400

    Fixed the list view button so it doesn't crash the app with multiple uses

--- app/src/main/java/com/ccseevents/owl/CalendarViewEventList.java
+++ app/src/main/java/com/ccseevents/owl/CalendarViewEventList.java
@@ -2,13 +2,30 @@ package com.ccseevents.owl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class CalendarViewEventList extends AppCompatActivity {
+
+    public ImageButton ListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_view_event_list);
+
+        ImageButton ListView = (ImageButton)findViewById(R.id.ListBttn);
+
+        ListView.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent intent = new Intent(CalendarViewEventList.this, EventListActivity.class);
+               intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+            }
+        });
     }
+
 }
