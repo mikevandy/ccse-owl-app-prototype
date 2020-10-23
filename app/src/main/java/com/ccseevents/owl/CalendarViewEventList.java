126ea78dcd73dbd28f4d68639dcc090f48537639
Scott <scottrobinson4591@gmail.com>
2020-10-22 20:53:28 -0400

    created toast message in order to test the date change listener

--- app/src/main/java/com/ccseevents/owl/CalendarViewEventList.java
+++ app/src/main/java/com/ccseevents/owl/CalendarViewEventList.java
@@ -1,22 +1,34 @@
 package com.ccseevents.owl;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CalendarView;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class CalendarViewEventList extends AppCompatActivity {
 
     public ImageButton ListView;
+    CalendarView calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_view_event_list);
-
+        CalendarView calendar = (CalendarView)findViewById(R.id.calendarView2);
         ImageButton ListView = (ImageButton)findViewById(R.id.ListBttn);
+
+
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(CalendarViewEventList.this, "Date Changed", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         ListView.setOnClickListener( new View.OnClickListener() {
             @Override
