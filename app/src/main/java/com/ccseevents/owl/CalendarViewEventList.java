5b79ac7536eb9bd6a9781c95ee4e610333c30ccf
Scott <scottrobinson4591@gmail.com>
2020-10-27 21:41:41 -0400

    Got recycler view working with "getAllData()". The items in the list are clickable just like they are in the event list activity

--- app/src/main/java/com/ccseevents/owl/CalendarViewEventList.java
+++ app/src/main/java/com/ccseevents/owl/CalendarViewEventList.java
@@ -23,6 +23,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
 public class CalendarViewEventList extends AppCompatActivity {
 
     private ImageButton ListView;
@@ -38,6 +39,8 @@ public class CalendarViewEventList extends AppCompatActivity {
 
         setContentView(R.layout.activity_calendar_view_event_list);
         rv = (RecyclerView) findViewById(R.id.recyclerView);
+
+
         CalendarView calendar = (CalendarView) findViewById(R.id.calendarView2);
         ListView = (ImageButton) findViewById(R.id.ListBttn);
 
@@ -49,18 +52,18 @@ public class CalendarViewEventList extends AppCompatActivity {
         date = formatter;
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        rv.setHasFixedSize(true);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         rv.setLayoutManager(layoutManager);
 
         final List<MyViewModel> calModel = new ArrayList<>();
+        calModel.addAll(generateSimpleList());
         MyAdapter adapter = new MyAdapter(calModel);
         rv.setAdapter(adapter);
 
         adapter.notifyDataSetChanged();
 
         Toast.makeText(CalendarViewEventList.this, date, Toast.LENGTH_SHORT).show();
-
-        calModel.addAll(generateSimpleList());
 
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
@@ -87,11 +90,58 @@ public class CalendarViewEventList extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        adapter.setOnItemClickListener(new MyAdapter.OnItemClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onItemClick(int position) {
+                Intent intent = new Intent(CalendarViewEventList.this, EventDetailActivity.class);
+                intent.putExtra("TITLE",calModel.get(position).getTitle());
+                intent.putExtra("HOST", calModel.get(position).getHost());
+                intent.putExtra("MONTH", calModel.get(position).getMonth());
+                intent.putExtra("DAY", calModel.get(position).getDay());
+                intent.putExtra("YEAR", calModel.get(position).getYear());
+                intent.putExtra("TIMEFROM", calModel.get(position).getFromTime());
+                intent.putExtra("TIMETO", calModel.get(position).getToTime());
+                intent.putExtra("LOCATION", calModel.get(position).getLocation());
+                intent.putExtra("DESCRIPTION", calModel.get(position).getDescription());
+                intent.putExtra("EVENTID", calModel.get(position).getId());
+                intent.putExtra("PHOTOURL", calModel.get(position).getPhotoURL());
+                //intent.putExtra("LISTTYPE",listType);
+                startActivity(intent);
+            }
+
+            @Override
+            public void toggleFav(int position) {
+                int eID = calModel.get(position).getId();
+                boolean favorited = myeventDB.existsMyEvents(eID);
+                if (favorited) {
+                    boolean isDeleted = myeventDB.deleteMyEvents(eID);
+                    if (isDeleted) {
+                        Toast.makeText(CalendarViewEventList.this, "Removed from My Events", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(CalendarViewEventList.this, "Failed to Remove", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else{
+                    boolean isInserted = myeventDB.insertMyEvents(eID);
+                    if (isInserted){
+                        Toast.makeText(CalendarViewEventList.this, "Added to My Events", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(CalendarViewEventList.this, "Failed to Add", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        });
+
     }
 
     private List<MyViewModel> generateSimpleList() {
         List<MyViewModel> myViewModelList = new ArrayList<>();
-        res = myeventDB.getEventsOnDate(date);
+        res = myeventDB.getAllData();
         if (res.getCount() != 0) {
             MyViewModel myViewModel[] = new MyViewModel[res.getCount()];
             int i = 0;
