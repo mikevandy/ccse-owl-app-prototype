018740a1b8978a74c151602f73202112c16e7bb2
mikevandy <mvander4@students.kennesaw.edu>
2020-10-29 18:03:51 -0400

    Added Main Menu Toolbar Added Toggle icon to Toolbar Added Navigation Drawer to Main Menu Linked Tool bar icor to Navigaton drawer Note Press nav icons specifically to go to activity Events, Myevents, Facebook icons work Calendar and Feedback icon works, but goes to empty page

---
+++ app/src/main/java/com/ccseevents/owl/navigation/calendar.java
@@ -0,0 +1,16 @@
+package com.ccseevents.owl.navigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ccseevents.owl.R;
+
+public class calendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+    }
+}
